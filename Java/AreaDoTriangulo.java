c86c43ca39aa53eda566a1b5ee2b695a69182970
kadusilva_14 <carloseduardodesouzasilva657@gmail.com>
2026-07-03 00:25:17 -0300

    Upload full project

--- Java/AreaDoTriangulo.java
+++ Java/AreaDoTriangulo.java
@@ -5,12 +5,12 @@ public class Main {
      public static void main(String[] args) {
          var baseYear = 2026;
          var scanner = new Scanner(System.in);
-         System.out.println("Informe o valor da base do Retangulo:");
+         System.out.println("Informe o valor da base do Triângulo:");
          var base = scanner.nextInt();
-         System.out.println("Informe o valor da altura do Retangulo");
+         System.out.println("Informe o valor da altura do Triângulo");
          var altura = scanner.nextInt();
          var area = (base * altura)/2;
-         System.out.printf("A area do quadrado e: %s", area);
+         System.out.printf("A area do triângulo e: %s", area);
 
      }
 }
